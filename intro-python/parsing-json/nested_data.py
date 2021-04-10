e5e2c26d33d2bce867cabca5ec3e6e2dc61a0538
stevenkitavi <steven.kitavi@gmail.com>
2021-04-10 05:34:25 -0700

    Meraki API methods interaction LAB

--- intro-python/parsing-json/nested_data.py
+++ intro-python/parsing-json/nested_data.py
@@ -8,10 +8,13 @@ import os
 
 # Get the absolute path for the directory where this file is located "here"
 here = os.path.abspath(os.path.dirname(__file__))
+print(here)
 
 
 with open(os.path.join(here, "interfaces.json")) as file:
     # TODO: Parse the contents of the JSON file into a variable
+    filepy = json.loads(file)
+    print(filepy)
 
 
 # TODO: Loop through the interfaces in the JSON data and print out each
